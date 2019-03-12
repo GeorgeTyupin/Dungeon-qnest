a2bf3b190af17cecea65b457bdbe621bd0bd7c1d
GeorgeTyupin <44722304+GeorgeTyupin@users.noreply.github.com>
2019-03-12 17:35:30 +0300

    Add files via upload

---
+++ Trader.pas
@@ -0,0 +1,32 @@
+{var mageHP,vraghp,magedam,mana, people,vamp,chance,x,yworot,elikmana,elikHP,chancenap,ygroza,lvl,MAXmageHP,MAXmana,danger,spusk,vragdam, vragdabldam,y,op, vragyv, vragvamp, pecp,
+chencevragyv,chancechance,chencevragdabldam,chanceyworot,helpDodge, helpEnemyDableDam,ydar, Gold, trader, traderСhoice:integer;}
+var Gold, saleSword:integer;
+
+procedure tradersShop;
+var trader, traderСhoice, loot:integer;
+begin
+writeln('Здравствуй, ты хотел что-то купить? (Если ты действительно хотел что-то купить нажми 1, если нет то 2');
+readln(traderСhoice);
+
+case traderСhoice of
+1:begin writeln(' Что ты хочешь купить? Уменя есть меч (нажми 1), броня (нажми 2), пузырёк с жизнями (нажми 3), пузырёк с маной (нажми 4).');
+
+case loot of
+1: if  Gold>=saleSword begin 
+writeln('Ты купил меч');
+magedam:=round(magedam*1,5)
+end;
+
+end;
+
+2:writeln('Возвращайся ещё');
+end;
+
+end;
+
+begin
+
+Gold:=1;
+
+ end.
+
